e1d3bd6dd1c15859f27bd523f96c55faf0640aee
Phil Schatzmann <phil_schatzmann@yahoo.com>
2019-10-22 05:13:28 +0200

    Allow remote jmx

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# mvn exec:java -Dexec.mainClass="ch.pschatzmann.jflightcontroller4pi.Main"
+
+# build the jar if it does not exist
+if [ ! -f ./target/jflightcontroller-0.0.1-SNAPSHOT-jar-with-dependencies.jar ]; then
+	mvn -Dmaven.test.skip=true package
+fi
+
+# run the jar
 java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -jar ./target/jflightcontroller-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
