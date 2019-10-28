29bd1a1ee241f635f4e6b6116d4357372f7ce2f1
Phil Schatzmann <phil_schatzmann@yahoo.com>
2019-10-28 17:02:58 +0100

    remove path to flightgear

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build the jar if it does not exist
-if [ ! -f ./target/jflightcontroller-0.0.1-SNAPSHOT-jar-with-dependencies.jar ]; then
+if [ ! -f ./target/jflightcontroller4pi-0.0.1-SNAPSHOT-jar-with-dependencies.jar ]; then
 	echo "Building jars..."
 	mvn -Dmaven.test.skip=true package
 fi
@@ -12,5 +12,5 @@ INTERFACE=wlan0
 PORT=7002
 IP=$(ifconfig $INTERFACE | grep inet | grep -v inet6 | awk '{print $2}')
 echo "You can connect with your JMX console to $IP:$PORT"
-java -Dlogback.configurationFile=file:./logback.xml -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -jar ./target/jflightcontroller-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
+java -Dlogback.configurationFile=file:./logback.xml -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -jar ./target/jflightcontroller4pi-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
 
