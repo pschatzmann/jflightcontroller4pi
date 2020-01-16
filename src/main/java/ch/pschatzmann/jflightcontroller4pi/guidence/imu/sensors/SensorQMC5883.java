7e76f700c1bdf537c9da98d94122ba0d8a736f31
Phil Schatzmann <phil_schatzmann@yahoo.com>
2020-01-16 10:33:46 +0100

    %1

--- src/main/java/ch/pschatzmann/jflightcontroller4pi/guidence/imu/sensors/SensorQMC5883.java
+++ src/main/java/ch/pschatzmann/jflightcontroller4pi/guidence/imu/sensors/SensorQMC5883.java
@@ -74,6 +74,8 @@ public class SensorQMC5883 implements ISensor {
 		i2c.read(0x06, 1, status);
 		if ((status[0] & 1) == 1) {
 			i2c.read(0x00, 3, values);
+		} else {
+			log.info("SensorQMC5883 data not ready");
 		}
 		return values;
 	}
